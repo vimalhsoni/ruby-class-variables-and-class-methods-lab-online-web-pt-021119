de5bbd18c53e0171bb7d66e233c0f172c46aa5db
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-13 23:55:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+  # @@album_count = 0
+  # @@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    # @@album_count += 1
+  end
+
+end
