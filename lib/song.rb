65f358d40b3feb9a0d1946db1969a36ee870f6fa
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-14 00:25:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,8 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = 0
-  @@genres = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
